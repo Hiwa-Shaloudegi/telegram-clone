ae700ce7222f5df7e334c6a117c7ae1400320634
Hiwa Shalouedgi <hiwa.shaloudegi@gmail.com>
2025-12-22 13:09:08 +0330

    add router sample

---
+++ lib/core/constants/route_names.dart
@@ -0,0 +1,7 @@
+abstract class RouteNames {
+  static const String chats = '/';
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String error = '/error';
+}
