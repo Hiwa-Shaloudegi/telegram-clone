f1d09fb38d223c178fee1e312d8d87e2c17d15b6
Hiwa <hiwa.shaloudegi@gmail.com>
2026-07-14 02:19:16 +0330

    fix: date section in messages

--- lib/data/models/chat_list_item_model.dart
+++ lib/data/models/chat_list_item_model.dart
@@ -71,7 +71,7 @@ class ChatListItemModel {
       imageUrl: json['image_url'] as String?,
       isPublic: json['is_public'] as bool? ?? false,
       inviteLink: json['invite_link'] as String?,
-      updatedAt: DateTime.parse(json['updated_at'] as String),
+      updatedAt: DateTime.parse(json['updated_at'] as String).toLocal(),
       memberRole: json['member_role'] as String? ?? 'member',
       isPinned: json['is_pinned'] as bool? ?? false,
       isArchived: json['is_archived'] as bool? ?? false,
@@ -80,7 +80,7 @@ class ChatListItemModel {
       lastMessageContent: json['last_message_content'] as String?,
       lastMessageType: json['last_message_type'] as String?,
       lastMessageAt: json['last_message_at'] != null
-          ? DateTime.parse(json['last_message_at'] as String)
+          ? DateTime.parse(json['last_message_at'] as String).toLocal()
           : null,
       lastMessageSenderId: json['last_message_sender_id'] as String?,
       lastMessageSenderName: json['last_message_sender_name'] as String?,
