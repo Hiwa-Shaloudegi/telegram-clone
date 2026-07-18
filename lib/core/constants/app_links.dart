5617e0f66add6e2a61a27ab8866d35a2bf62a370
Hiwa <hiwa.shaloudegi@gmail.com>
2026-07-18 19:13:06 +0330

    fix: github link and saved message navigation in app drawer

---
+++ lib/core/constants/app_links.dart
@@ -0,0 +1,4 @@
+abstract class AppLinks {
+  static const String githubRepo =
+      'https://github.com/Hiwa-Shaloudegi/telegram-clone';
+}
--- lib/features/chat_list/ui/widgets/app_drawer.dart
+++ lib/features/chat_list/ui/widgets/app_drawer.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:telegram_clone/app/enums/chat_type.dart';
 import 'package:telegram_clone/app/router/extra/contacts_page_extra.dart';
+import 'package:telegram_clone/core/constants/app_links.dart';
 import 'package:telegram_clone/core/constants/route_names.dart';
+import 'package:telegram_clone/core/ui/widgets/app_snackbar.dart';
+import 'package:telegram_clone/features/chat_list/notifiers/query/watch_user_chats_query.dart';
+import 'package:telegram_clone/features/chat_list/notifiers/ui/main_ui_state.dart';
 import 'package:telegram_clone/features/chat_list/ui/widgets/drawer_headerd.dart';
 import 'package:telegram_clone/features/chat_list/ui/widgets/drawer_item_tile.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
+
+  void _openSavedMessages(BuildContext context, WidgetRef ref) {
+    final chats = ref.read(watchUserChatsQueryProvider).value;
+    final saved = chats
+        ?.where((c) => c.chatType == ChatType.saved)
+        .firstOrNull;
+
+    if (saved == null) {
+      AppSnackbar.showError(
+        context,
+        'No saved messages yet. Save a message to start.',
+      );
+      return;
+    }
+
+    ref.read(mainUi_selectedChatItemProviderProvider.notifier).set(saved);
+    Navigator.pop(context);
+    context.pushNamed(
+      RouteNames.chat,
+      pathParameters: {'chatId': saved.chatId},
+      extra: saved,
+    );
+  }
+
+  Future<void> _openGithub(BuildContext context) async {
+    final uri = Uri.parse(AppLinks.githubRepo);
+    final launched = await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+    );
+    if (!launched && context.mounted) {
+      AppSnackbar.showError(context, 'Could not open the repository.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,9 +84,12 @@ class AppDrawer extends StatelessWidget {
                 },
               ),
 
-              DrawerItemTile(
-                title: 'Saved Messages',
-                icon: Icons.bookmark_outline,
+              Consumer(
+                builder: (context, ref, _) => DrawerItemTile(
+                  title: 'Saved Messages',
+                  icon: Icons.bookmark_outline,
+                  onTap: () => _openSavedMessages(context, ref),
+                ),
               ),
 
               DrawerItemTile(
@@ -65,9 +109,7 @@ class AppDrawer extends StatelessWidget {
               DrawerItemTile(
                 title: 'Github',
                 icon: Icons.code,
-                onTap: () {
-                  // TODO: Add repo link
-                },
+                onTap: () => _openGithub(context),
               ),
             ],
           ),
